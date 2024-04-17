73db732bf7239306a8f8aadbf40626b2ae24c76d
Anna Celina <anna.celina0000@gmail.com>
2024-04-18 00:02:09 +0300

    feat: connect Cognito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import styles from "./page.module.css";
+import "@aws-amplify/ui-react/styles.css";
+
+import { Amplify } from "aws-amplify";
+import config from "../amplifyconfiguration.json";
+import {
+  WithAuthenticatorProps,
+  withAuthenticator,
+} from "@aws-amplify/ui-react";
+
+Amplify.configure(config);
+
+const Home = ({ signOut, user }: WithAuthenticatorProps) => {
   return (
     <main className={styles.main}>
       <p>My amplify project</p>
+      <p>{`User: ${user?.username}`}</p>
     </main>
   );
-}
+};
+
+export default withAuthenticator(Home);
