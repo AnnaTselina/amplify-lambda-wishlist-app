da558b64a0b769a394f387089bbde57eb0220dad
Anna Celina <anna.celina0000@gmail.com>
2024-04-18 17:57:55 +0300

    feat: add call to API handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,34 @@ import { Header } from "./components";
 Amplify.configure(config);
 
 const Home = ({ user }: WithAuthenticatorProps) => {
+  const handleCreateWishlist = () => {
+    // instantiate a headers object
+    const myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+
+    // create a JSON object with parameters for API call and store in a variable
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      // redirect: "follow",
+    };
+
+    fetch(
+      "https://6zmhwmkyyg.execute-api.eu-north-1.amazonaws.com/dev",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => alert(JSON.parse(result).body))
+      .catch((error) => console.log("error", error));
+  };
+
   return (
     <>
       <Header />
       <main className={styles.main}>
         <p>My amplify project</p>
+        <button onClick={handleCreateWishlist}>Create wishlist</button>
         <p>{`User: ${user?.username}`}</p>
       </main>
     </>
